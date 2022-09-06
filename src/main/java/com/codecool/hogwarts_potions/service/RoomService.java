d23624cd051af1c9574386f0946cfe346533fae5
reka <debreka@gmail.com>
2022-09-06 15:46:40 +0200

    add methods to Roomservice

--- src/main/java/com/codecool/hogwarts_potions/service/RoomService.java
+++ src/main/java/com/codecool/hogwarts_potions/service/RoomService.java
@@ -1,20 +1,23 @@
 package com.codecool.hogwarts_potions.service;
 
 import com.codecool.hogwarts_potions.model.Room;
+import com.codecool.hogwarts_potions.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RoomService {
+    @Autowired
+    private RoomRepository roomRepository;
 
     public List<Room> getAllRooms() {
-        //TODO
-        return null;
+        return roomRepository.findAll();
     }
 
     public void addRoom(Room room) {
-        //TODO
+       roomRepository.saveAndFlush(room);
     }
 
     public Room getRoomById(Long id) {
